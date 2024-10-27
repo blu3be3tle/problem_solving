701733e25f0fc0006d26a88fefedbaa55c31835a
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-10-27 21:12:27 +0600

    w1d1: sereja and dima solve

---
+++ xpsc/week1/day1/A_Sereja_and_Dima.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    fastIO;
+    int n;
+    cin >> n;
+    deque<int> dq(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> dq[i];
+    }
+
+    int s = 0, d = 0, who = 1;
+
+    while (!dq.empty())
+    {
+        int left = dq.front(), right = dq.back(), mx;
+        mx = max(left, right);
+        if (who % 2 != 0)
+        {
+            s += mx;
+        }
+        else
+        {
+            d += mx;
+        }
+        if (mx == left)
+        {
+            dq.pop_front();
+        }
+        else
+        {
+            dq.pop_back();
+        }
+        who++;
+    }
+    cout << s << " " << d << endl;
+
+    return 0;
+}
