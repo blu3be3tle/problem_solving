a12fd52493cee1a6e605342c0cdbf16b9548ad52
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-10-30 05:01:01 +0600

    w1d4: radio station solve

---
+++ xpsc/week1/day4/B_Radio_Station.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    fastIO;
+    int n, m;
+    cin >> n >> m;
+    unordered_map<string, string> server;
+
+    for (int i = 0; i < n; i++)
+    {
+        string name, ip;
+        cin >> name >> ip;
+        server[ip] = name;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        string cmd, ip;
+        cin >> cmd >> ip;
+        ip.pop_back();
+        cout << cmd << " " << ip << "; #" << server[ip] << endl;
+    }
+
+    return 0;
+}
