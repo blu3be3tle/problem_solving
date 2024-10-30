cb936483e2512ce574135ab6ce7020c8a9fe4e74
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-10-30 19:24:35 +0600

    w1d4: tom riddle's diary solve

---
+++ xpsc/week1/day4/A_Tom_Riddle_s_Diary.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define endl '\n'
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    fastIO;
+    int n;
+    cin >> n;
+    set<string> found;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+
+        if (found.find(s) != found.end())
+        {
+            cout << "YES" << endl;
+        }
+        else
+            cout << "NO" << endl;
+            found.insert(s);
+    }
+    return 0;
+}
