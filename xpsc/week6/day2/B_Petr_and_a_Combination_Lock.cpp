5a473a554cd8e62009e6424ddc9103ce90058f0c
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-12-10 23:56:02 +0600

    w6d2 code update

---
+++ xpsc/week6/day2/B_Petr_and_a_Combination_Lock.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main()
+{
+    fastIO;
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    bool ok = false;
+    for (int mask = 0; mask < (1 << n); mask++)
+    {
+        int sum = 0;
+        for (int k = 0; k < n; k++)
+        {
+            if ((mask >> k) & 1)
+            {
+                sum += a[k];
+            }
+            else
+            {
+                sum -= a[k];
+            }
+        }
+
+        if (sum % 360 == 0)
+        {
+            ok = true;
+            break;
+        }
+    }
+
+    if (ok)
+        yes;
+    else
+        no;
+    return 0;
+}
