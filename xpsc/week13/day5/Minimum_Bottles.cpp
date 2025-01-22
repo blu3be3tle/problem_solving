e0a477c33dc7bc19c7b7bbb6f67258a376924eee
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-01-22 23:36:40 +0600

    w13d5 code update

---
+++ xpsc/week13/day5/Minimum_Bottles.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+using namespace std;
+#define endl '\n'
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main()
+{
+    fastIO;
+    tc
+    {
+        int n, x;
+        cin >> n >> x;
+        vector<int> a(n);
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            sum += a[i];
+        }
+        cout << (sum + x - 1) / x << endl;
+    }
+    return 0;
+}
