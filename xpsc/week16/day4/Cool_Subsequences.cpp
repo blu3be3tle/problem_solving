acbf9df78f12fb5a60fea01b99a801c2d6061ccc
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-02-12 23:34:16 +0600

    w16d4 code update

---
+++ xpsc/week16/day4/Cool_Subsequences.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+using namespace std;
+#define endl '\n'
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    unordered_map<int, int> freq;
+    int found = -1;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+        freq[a[i]]++;
+        if (freq[a[i]] >= 2)
+        {
+            found = a[i];
+        }
+    }
+
+    if (found != -1)
+    {
+        cout << "1\n"
+             << found << "\n";
+    }
+    else
+    {
+        cout << "-1\n";
+    }
+}
+
+int main()
+{
+    fastIO;
+    tc
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ xpsc/week16/day4/Time_Penalty.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+using namespace std;
+#define endl '\n'
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main()
+{
+    fastIO;
+
+    int x, y;
+    cin >> x >> y;
+    cout << x + (y * 10) << endl;
+    return 0;
+}
