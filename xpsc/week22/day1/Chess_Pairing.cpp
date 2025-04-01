0862147aa9043911920c38c29e3d5d75f26fd7a0
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-04-02 02:08:42 +0600

    w22 d1 code update

---
+++ xpsc/week22/day1/Chess_Pairing.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+using namespace std;
+#define endl '\n'
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main()
+{
+    fastIO;
+    tc
+    {
+        int n, x;
+        cin >> n >> x;
+
+        cout << max(0, (x - (2 * n - x))) << endl;
+    }
+    return 0;
+}
